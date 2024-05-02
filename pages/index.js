1dab1b4e55d6ebb914f345c3f7c29d2bfe63e72a
Sover <89106761+TheOfGamer@users.noreply.github.com>
2024-05-02 22:19:29 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,7 @@ export default function Home() {
         formData.append('outputType', outputType);
 
         try {
-            const response = await axios.post('/convert', formData, {
+            const response = await axios.post('https://cloud.sovagroup.one/convert', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
